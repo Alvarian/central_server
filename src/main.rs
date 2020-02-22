02b26ba3160b2c733e9658b756ad52a5afd2b7cd
alvarian <vitarka88@gmail.com>
2020-02-22 10:22:37 -0500

    cors setup, env setup. api connection to front end build

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(plugin, const_fn, proc_macro_hygiene, decl_macro)]
 use rocket::*;
+use rocket_cors;
+use rocket::http::Method; // 1
 
 #[macro_use]
 extern crate diesel;
@@ -23,6 +25,36 @@ mod db;
 mod routes;
 use routes::*;
 
+use rocket_cors::{
+    AllowedHeaders, AllowedOrigins, Error, // 2.
+    Cors, CorsOptions // 3.
+};
+
+
+fn make_cors() -> Cors {	
+	dotenv().ok();
+
+	// WILL BE LIST OF ADDRESS ROOTS. UPDATE THIS WHEN I KNOW HOW TO MAP LIST OF ENVS AS ARRAY
+	let portfolio_address = env::var("PORTFOLIO_ROOT_ADDRESS").expect("set PORTFOLIO_ROOT_ADDRESS");
+    
+    let allowed_origins = AllowedOrigins::some_exact(&[ // 4.
+        portfolio_address,
+    ]);
+
+    CorsOptions { // 5.
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(), // 1.
+        allowed_headers: AllowedHeaders::some(&[
+            "Authorization",
+            "Accept",
+            "Access-Control-Allow-Origin", // 6.
+        ]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("error while building CORS")
+}
 
 fn rocket() -> rocket::Rocket {
 	dotenv().ok();
@@ -42,8 +74,11 @@ fn rocket() -> rocket::Rocket {
 			],
 		)
 		.register(catchers![portfolio::not_found])
+		.attach(make_cors())
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
 	rocket().launch();
+
+	Ok(())
 }
